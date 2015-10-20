583482d8ac6b47a45b63a47407a0473d4594e7af
Pablo <Pablo-G@users.noreply.github.com>
2015-10-20 00:42:49 -0500

    Funcionalidades Añadidas

--- src/JukeBox/ControladorApp.java
+++ src/JukeBox/ControladorApp.java
@@ -3,6 +3,7 @@ package JukeBox;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.CheckBox;
 import javafx.event.ActionEvent;
 import javafx.stage.FileChooser;
 import java.io.File;
@@ -73,16 +74,84 @@ public class ControladorApp{
     @FXML private TextArea agBaBio;
     @FXML private Text agBaErr;
 
-    @FXML protected void agregaBanda(ActionEvent event) throws Exception{
+    @FXML protected void agregaBanda(ActionEvent event){
     	if (bandaIm == null || agBaNom.getText().equals("") || agBaBio.getText().equals("")) {
     		agBaErr.setText("Debes llenar todos los campos!");
     		return;
     	}
-    	cBD.agregaBanda(agBaNom.getText(), bandaIm.toString(), agBaBio.getText());
+    	try{
+    		cBD.agregaBanda(agBaNom.getText(), bandaIm.toString(), agBaBio.getText());
+    	}catch(Exception e){
+    		agBaErr.setText(e.getMessage());
+    		agBaNom.setText("");
+    		agBaBio.setText("");
+    		bandaIm = null;
+    		return;
+    	}
     	agBaNom.setText("");
     	agBaBio.setText("");
     	bandaIm = null;
     	agBaErr.setText("");
+    }
+
+	@FXML private TextField agArNom;
+	@FXML private TextField agArInt;
+    @FXML private TextArea agArBio;
+    @FXML private Text agArErr;
+
+    @FXML protected void agregaArtista(ActionEvent event){
+    	if (artisIm == null || agArNom.getText().equals("") || agArInt.getText().equals("") || agArBio.getText().equals("")) {
+    		agArErr.setText("Debes llenar todos los campos!");
+    		return;
+    	}
+    	try{
+    		cBD.agregaArtista(agArNom.getText(), artisIm.toString(), agArBio.getText(), agArInt.getText());
+    	}catch(Exception e){
+    		agArErr.setText(e.getMessage());
+    		agArNom.setText("");
+    		agArInt.setText("");
+    		agArBio.setText("");
+    		artisIm = null;
+    		return;
+    	}
+    	agArNom.setText("");
+    	agArInt.setText("");
+    	agArBio.setText("");
+    	artisIm = null;
+    	agArErr.setText("");
+    }
+
+	@FXML private TextField agAlNom;
+	@FXML private TextField agAlInt;
+	@FXML private TextField agAlAno;
+	@FXML private TextField agAlDis;
+    @FXML private CheckBox agAlArt;
+    @FXML private Text agAlErr;
+
+    @FXML protected void agregaAlbum(ActionEvent event){
+    	if (albumIm == null || agAlNom.getText().equals("") || agAlInt.getText().equals("") || agAlAno.getText().equals("") || agAlDis.getText().equals("")) {
+    		agAlErr.setText("Debes llenar todos los campos!");
+    		return;
+    	}
+    	try{
+    		cBD.agregaAlbum(agAlNom.getText(), albumIm.toString(), agAlInt.getText(), Integer.parseInt(agAlAno.getText()), Integer.parseInt(agAlDis.getText()), agAlArt.isSelected());
+    	}catch(Exception e){
+    		agAlErr.setText(e.getMessage());
+    		agAlNom.setText("");
+    		agAlInt.setText("");
+    		agAlAno.setText("");
+    		agAlDis.setText("");
+    		agAlArt.setSelected(false);
+    		albumIm = null;
+    		return;
+    	}
+    	agAlNom.setText("");
+    	agAlInt.setText("");
+    	agAlAno.setText("");
+    	agAlDis.setText("");
+    	agAlArt.setSelected(false);
+    	albumIm = null;
+    	agAlErr.setText("");
     }
 
     @FXML private TextField agCaNom;
@@ -94,19 +163,51 @@ public class ControladorApp{
     @FXML private TextField agCaCal;
     @FXML private TextField agCaPis;
     @FXML private TextField agCaDis;
+    @FXML private CheckBox agCaSen;
+	@FXML private Text agCaErr;
 
     @FXML protected void agregaCancion(ActionEvent event){
-
-	    agCaNom.setText("Dime Ven");
-	    agCaAlb.setText("Best Of");
-	    agCaInt.setText("Motel");
-	    agCaCom.setText("Juan");
-	    agCaGen.setText("Pop");
-	    agCaAno.setText("2010");
-	    agCaCal.setText("5");
-	    agCaPis.setText("1");
-	    agCaDis.setText("1");
-
+    	if (canciAr == null || agCaNom.getText().equals("") || agCaAlb.getText().equals("") || 
+    		agCaInt.getText().equals("") || agCaCom.getText().equals("") ||
+    		agCaGen.getText().equals("") || agCaAno.getText().equals("") ||
+    		agCaCal.getText().equals("") || agCaPis.getText().equals("") ||
+    		agCaDis.getText().equals("")) {
+    		agCaErr.setText("Debes llenar todos los campos!");
+    		return;
+    	}
+    	try{
+    		cBD.agregaCancion(agCaNom.getText(), agCaAlb.getText(), 
+    						  agCaInt.getText(), agCaCom.getText(),
+    						  agCaGen.getText(), Integer.parseInt(agCaAno.getText()),
+    						  Integer.parseInt(agCaCal.getText()), Integer.parseInt(agCaPis.getText()),
+    						  Integer.parseInt(agCaDis.getText()), agCaSen.isSelected(), canciAr.toString());
+    	}catch(Exception e){
+    		agCaErr.setText(e.getMessage());
+    		agCaNom.setText("");
+    		agCaAlb.setText("");
+    		agCaInt.setText("");
+    		agCaCom.setText("");
+    		agCaGen.setText("");
+    		agCaAno.setText("");
+    		agCaCal.setText("");
+    		agCaPis.setText("");
+    		agCaDis.setText("");
+    		agCaSen.setSelected(false);
+    		canciAr = null;
+    		return;
+    	}
+    	agCaNom.setText("");
+    	agCaAlb.setText("");
+   		agCaInt.setText("");
+    	agCaCom.setText("");
+    	agCaGen.setText("");
+   		agCaAno.setText("");
+    	agCaCal.setText("");
+    	agCaPis.setText("");
+   		agCaDis.setText("");
+    	agCaSen.setSelected(false);
+    	canciAr = null;
+    	agCaErr.setText("");
     }
 
 }
--- src/JukeBox/ControladorBD.java
+++ src/JukeBox/ControladorBD.java
@@ -2,6 +2,7 @@ package JukeBox;
 
 import java.sql.*;
 import java.io.File;
+import java.util.Calendar;
 
 public class ControladorBD{
 	
@@ -36,42 +37,42 @@ public class ControladorBD{
                    " Reproducciones  INT                NOT NULL," + 
                    " FechaIncl       DATE               NOT NULL," + 
                    " Ubicacion       TEXT               NOT NULL," + 
-                   " Ruta            TEXT               NOT NULL)"; 
+                   " Ruta            TEXT               NOT NULL);"; 
       		statem.executeUpdate(sql);
 	    	sql = "CREATE TABLE Albumes" +
                    "(Nombre          TEXT  PRIMARY KEY  NOT NULL," +
                    " Ano             INT                NOT NULL," + 
                    " NumDiscos       INT                NOT NULL," + 
                    " NumCanciones    INT                NOT NULL," + 
-                   " Ilustracion     TEXT               NOT NULL)"; 
+                   " Ilustracion     TEXT               NOT NULL);"; 
       		statem.executeUpdate(sql);
       		sql = "CREATE TABLE Artistas" +
                    "(Nombre          TEXT  PRIMARY KEY  NOT NULL," +
                    " Ilustracion     TEXT               NOT NULL," + 
-                   " Biografia       TEXT               NOT NULL)"; 
+                   " Biografia       TEXT               NOT NULL);"; 
       		statem.executeUpdate(sql);
       		sql = "CREATE TABLE Bandas" +
                    "(Nombre          TEXT  PRIMARY KEY  NOT NULL," +
                    " Ilustracion     TEXT               NOT NULL," + 
-                   " Biografia       TEXT               NOT NULL)"; 
+                   " Biografia       TEXT               NOT NULL);"; 
       		statem.executeUpdate(sql);
       		sql = "CREATE TABLE IntegranteDe" +
                    "(ArtistaNombre   TEXT  NOT NULL," +
                    " BandaNombre     TEXT  NOT NULL, " +
                    " FOREIGN KEY(ArtistaNombre) REFERENCES Artistas(Nombre)" +	
-                   " FOREIGN KEY(BandaNombre) REFERENCES Bandas(Nombre))"; 
+                   " FOREIGN KEY(BandaNombre) REFERENCES Bandas(Nombre));"; 
       		statem.executeUpdate(sql);
       		sql = "CREATE TABLE InterpretadaPor" +
                    "(CancionNombre   TEXT  NOT NULL," +
                    " ArtistaNombre   TEXT  NOT NULL, " +
                    " FOREIGN KEY(CancionNombre) REFERENCES Canciones(Nombre)" +	
-                   " FOREIGN KEY(ArtistaNombre) REFERENCES Artistas(Nombre))"; 
+                   " FOREIGN KEY(ArtistaNombre) REFERENCES Artistas(Nombre));"; 
       		statem.executeUpdate(sql);
       		sql = "CREATE TABLE AlbumDe" +
                    "(AlbumNombre   TEXT  NOT NULL," +
                    " ArtistasNombre     TEXT  NOT NULL, " +
                    " FOREIGN KEY(AlbumNombre) REFERENCES Albumes(Nombre)" +	
-                   " FOREIGN KEY(ArtistasNombre) REFERENCES Artistas(Nombre))"; 
+                   " FOREIGN KEY(ArtistasNombre) REFERENCES Artistas(Nombre));"; 
       		statem.executeUpdate(sql);
       		sql = "CREATE TABLE PerteneceA" +
                    "(CancionNombre   TEXT  NOT NULL," +
@@ -79,7 +80,7 @@ public class ControladorBD{
                    " NumPista        INT                NOT NULL," + 
                    " NumDisco        INT                NOT NULL," + 
                    " FOREIGN KEY(CancionNombre) REFERENCES Canciones(Nombre)" +	
-                   " FOREIGN KEY(AlbumNombre) REFERENCES Albumes(Nombre))"; 
+                   " FOREIGN KEY(AlbumNombre) REFERENCES Albumes(Nombre));"; 
       		statem.executeUpdate(sql);
 	    }
 
@@ -89,9 +90,13 @@ public class ControladorBD{
 
 	public void agregaBanda(String nombre, String ilustr, String biogra) throws Exception{
 		String sql = "INSERT INTO Bandas (Nombre,Ilustracion,Biografia) " +
-        	"VALUES ('" + nombre + "', '" + ilustr + "', '" + biogra + "')"; 
+        	"VALUES ('" + nombre + "', '" + ilustr + "', '" + biogra + "');"; 
 
-	    statem.executeUpdate(sql);
+        try{
+	    	statem.executeUpdate(sql);
+	    }catch(Exception e){
+	    	throw new Exception(nombre + " ya se encuentra registrado en la Base de Datos!");
+	    }
 	    conect.commit();
 
     	imprimeTablas();
@@ -99,13 +104,135 @@ public class ControladorBD{
 
 	public void agregaArtista(String nombre, String ilustr, String biogra, String inteDe) throws Exception{
 
-		String sql = "INSERT INTO Artistas (Nombre,Ilustracion,Biografia) " +
-        	"VALUES ('" + nombre + "', '" + ilustr + "', '" + biogra + "')"; 
+	    String[] inteDeA = inteDe.split(";");
 
-	    statem.executeUpdate(sql);
+	    for (String s : inteDeA) {
+	    	ResultSet rs = statem.executeQuery("SELECT Nombre FROM Bandas WHERE Nombre LIKE '" + s + "';");
+	    	if(!rs.next()){
+	    		throw new Exception(s + " no ha sido registrado en la Base de Datos!");
+	    	}
+	    	rs.close();
+	    }
+
+		String sql = "INSERT INTO Artistas (Nombre,Ilustracion,Biografia) " +
+        	"VALUES ('" + nombre + "', '" + ilustr + "', '" + biogra + "');"; 
+
+       	try{
+	    	statem.executeUpdate(sql);
+	    }catch(Exception e){
+	    		throw new Exception(nombre + " ya se encuentra registrado en la Base de Datos!");
+	    }
 	    conect.commit();
 
+	    for (String s : inteDeA) {
+			sql = "INSERT INTO IntegranteDe (ArtistaNombre,BandaNombre) " +
+	        	"VALUES ('" + nombre + "', '" + s + "')"; 
+		    statem.executeUpdate(sql);
+		    conect.commit();    	
+	    }
+
     	imprimeTablas();
+	}
+
+	public void agregaAlbum(String nombre, String ilustr, String interp, int ano, int dis, boolean artist) throws Exception{
+
+		if (artist) {
+	    	ResultSet rs = statem.executeQuery("SELECT Nombre FROM Artistas WHERE Nombre LIKE '" + interp + "';");
+	    	if(!rs.next()){
+	    		throw new Exception(interp + " no ha sido registrado en la Base de Datos!");
+	    	}
+	    	rs.close();			
+		}else{
+	    	ResultSet rs = statem.executeQuery("SELECT Nombre FROM Bandas WHERE Nombre LIKE '" + interp + "';");
+	    	if(!rs.next()){
+	    		throw new Exception(interp + " no ha sido registrado en la Base de Datos!");
+	    	}
+	    	rs.close();				
+		}
+
+
+		String sql = "INSERT INTO Albumes (Nombre,Ano,NumDiscos,NumCanciones,Ilustracion) " +
+        	"VALUES ('" + nombre + "', " + ano + ", " + dis + ", 0, '" +  ilustr + "');"; 
+
+       	try{
+	    	statem.executeUpdate(sql);
+	    }catch(Exception e){
+	    		throw new Exception(nombre + " ya se encuentra registrado en la Base de Datos!");
+	    }
+	    conect.commit();
+
+		sql = "INSERT INTO AlbumDe (AlbumNombre,ArtistasNombre) " +
+	    	"VALUES ('" + nombre + "', '" + interp + "')"; 
+		statem.executeUpdate(sql);
+		conect.commit();    	
+
+    	imprimeTablas();
+	}
+
+	public void agregaCancion(String nombre, String album, 
+    						  String inteDe, String compo,
+    						  String genero, int ano,
+    						  int califi, int pista,
+    						  int disco , boolean sencillo,
+    						  String ubica ) throws Exception{
+		Calendar calen = Calendar.getInstance();
+		String fechaIncl = "'" + calen.YEAR + "-" + calen.MONTH + "-" + calen.DAY_OF_MONTH + " " + calen.HOUR_OF_DAY + ":" + calen.MINUTE + ":00'";
+
+	    String[] inteDeC = inteDe.split(";");
+
+	    for (String s : inteDeC) {
+	    	ResultSet rsa = statem.executeQuery("SELECT Nombre FROM Artistas WHERE Nombre LIKE '" + s + "';");
+	    	ResultSet rsb = statem.executeQuery("SELECT Nombre FROM Bandas WHERE Nombre LIKE '" + s + "';");
+
+	    	if(!rsa.next() && !rsb.next()){
+	    		throw new Exception(s + " no ha sido registrado en la Base de Datos!");
+	    	}
+	    	rsa.close();
+	    	rsb.close();
+	    }
+
+		if (!sencillo) {
+	    	ResultSet rs = statem.executeQuery("SELECT Nombre FROM Albumes WHERE Nombre LIKE '" + album + "';");
+	    	if(!rs.next()){
+	    		throw new Exception(album + " no ha sido registrado en la Base de Datos!");
+	    	}
+	    	rs.close();	
+		}
+
+		String sql = "INSERT INTO Canciones (Nombre,Compositor,Genero,Ano,Calificacion,Reproducciones,FechaIncl,Ubicacion,Ruta) " +
+        	"VALUES ('" + nombre + "', '" + compo + "', '" + genero + "', " + ano + ", " + califi + ", 0, " + fechaIncl + ", 'Local', '" + ubica + "');";
+
+       	try{
+	    	statem.executeUpdate(sql);
+	    }catch(Exception e){
+	    		throw e;
+	    		//throw new Exception(nombre + " ya se encuentra registrado en la Base de Datos!");
+	    }
+	    conect.commit();
+
+	    for (String s : inteDeC) {
+			sql = "INSERT INTO InterpretadaPor (CancionNombre,ArtistaNombre) " +
+	        	"VALUES ('" + nombre + "', '" + s + "')"; 
+		    statem.executeUpdate(sql);
+		    conect.commit();    	
+	    }
+
+	    if (!sencillo) {
+			sql = "INSERT INTO PerteneceA (CancionNombre,AlbumNombre,NumPista,NumDisco) " +
+	        	"VALUES ('" + nombre + "', '" + album + "', " + pista + ", " + disco + ")";
+		    statem.executeUpdate(sql);
+		    conect.commit();
+
+	    	ResultSet rsc = statem.executeQuery("SELECT NumCanciones FROM Albumes WHERE Nombre LIKE '" + album + "';");
+	    	int numc = rsc.getInt("NumCanciones");
+		    rsc.close();
+	    	numc++;
+
+		    sql = "UPDATE Albumes SET NumCanciones = " + numc + " WHERE Nombre=" + album + ";";
+		    statem.executeUpdate(sql);
+		    conect.commit();
+	    }
+		
 	}
 
 	public void imprimeTablas(){
@@ -134,6 +261,34 @@ public class ControladorBD{
 	        System.out.println();
 	    	}
 	    	rs.close();
+	    	rs = statem.executeQuery( "SELECT * FROM Albumes" );
+	    	System.out.println("Albumes");
+	    	while (rs.next()) {
+	        	String nombreal = rs.getString("Nombre");
+	        	int anoal = rs.getInt("Ano");
+	        	int anodis = rs.getInt("NumDiscos");
+	        	int numcan = rs.getInt("NumCanciones");
+	        	String ilustral = rs.getString("Ilustracion");
+	        System.out.println( "Nombre = " + nombreal );
+	        System.out.println( "Ano = " + anoal );
+	        System.out.println( "NumDiscos = " + anodis );
+	        System.out.println( "NumCanciones = " + numcan );
+	        System.out.println( "Ilustracion = " + ilustral );
+	        System.out.println();
+	    	}
+	    	rs.close();
+	    	rs = statem.executeQuery( "SELECT * FROM Artistas" );
+	    	System.out.println("Artistas");
+	    	while (rs.next()) {
+	        	String nombrea = rs.getString("Nombre");
+	        	String ilustraciona = rs.getString("Ilustracion");
+	        	String biografiaa = rs.getString("Biografia");
+	        System.out.println( "Nombre = " + nombrea );
+	        System.out.println( "Ilustracion = " + ilustraciona );
+	        System.out.println( "Biografia = " + biografiaa );
+	        System.out.println();
+	    	}
+	    	rs.close();
 	    	rs = statem.executeQuery( "SELECT * FROM Bandas" );
 	    	System.out.println("Bandas");
 	    	while (rs.next()) {
@@ -143,6 +298,50 @@ public class ControladorBD{
 	        System.out.println( "Nombre = " + nombreb );
 	        System.out.println( "Ilustracion = " + ilustracion );
 	        System.out.println( "Biografia = " + biografia );
+	        System.out.println();
+	    	}
+	    	rs.close();
+	    	rs = statem.executeQuery( "SELECT * FROM IntegranteDe" );
+	    	System.out.println("IntegranteDe");
+	    	while (rs.next()) {
+	        	String nombrear = rs.getString("ArtistaNombre");
+	        	String nombreba = rs.getString("BandaNombre");
+	        System.out.println( "ArtistaNombre = " + nombrear );
+	        System.out.println( "BandaNombre = " + nombreba );
+	        System.out.println();
+	    	}
+	    	rs.close();
+	    	rs = statem.executeQuery( "SELECT * FROM AlbumDe" );
+	    	System.out.println("AlbumDe");
+	    	while (rs.next()) {
+	        	String nombrealb = rs.getString("AlbumNombre");
+	        	String nombreart = rs.getString("ArtistasNombre");
+	        System.out.println( "AlbumNombre = " + nombrealb );
+	        System.out.println( "ArtistasNombre = " + nombreart );
+	        System.out.println();
+	    	}
+	    	rs.close();
+	    	rs = statem.executeQuery( "SELECT * FROM InterpretadaPor" );
+	    	System.out.println("InterpretadaPor");
+	    	while (rs.next()) {
+	        	String cancionom = rs.getString("CancionNombre");
+	        	String artistnom = rs.getString("ArtistaNombre");
+	        System.out.println( "CancionNombre = " + cancionom );
+	        System.out.println( "ArtistaNombre = " + artistnom );
+	        System.out.println();
+	    	}
+	    	rs.close();
+	    	rs = statem.executeQuery( "SELECT * FROM PerteneceA" );
+	    	System.out.println("PerteneceA");
+	    	while (rs.next()) {
+	        	String cancionnom = rs.getString("CancionNombre");
+	        	String artistanom = rs.getString("AlbumNombre");
+	        	int numpist = rs.getInt("NumPista");
+	        	int numdisc = rs.getInt("NumDisco");	        	
+	        System.out.println( "CancionNombre = " + cancionnom );
+	        System.out.println( "AlbumNombre = " + artistanom );
+	       	System.out.println( "NumPista = " + numpist );
+	        System.out.println( "NumDisco = " + numdisc );
 	        System.out.println();
 	    	}
 	    	rs.close();
