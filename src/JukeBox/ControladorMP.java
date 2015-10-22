6e3ef3510e43b1cc89c93e9aa65cff9be664e4bb
Pablo <Pablo-G@users.noreply.github.com>
2015-10-21 20:30:32 -0500

    Funcionalidad Añadida

--- src/JukeBox/ControladorMP.java
+++ src/JukeBox/ControladorMP.java
@@ -2,21 +2,24 @@ package JukeBox;
 
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import java.io.File;
 
 public class ControladorMP{
 
 	Media m;
 	MediaPlayer mp;
 
-	public ControladorMP(){
-		m = null;
-		mp = null;
-	}
-
 	public ControladorMP(String url){
-		m = new Media(url);
+		m = new Media(new File(url).toURI().toString());
 		mp = new MediaPlayer(m);
 	}
 
+	public void play(){
+		mp.play();
+	}
+
+	public void pausa(){
+		mp.pause();
+	}
 
 }
